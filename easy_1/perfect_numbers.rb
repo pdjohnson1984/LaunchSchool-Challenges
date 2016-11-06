2e2fb71d08ad97b9a615e1719720f1bc89cd5770
Phillip Johnson <pdjohnson84@gmail.com>
2016-11-06 15:12:18 -0800

    refactoring perfect numbers

--- easy_1/perfect_numbers.rb
+++ easy_1/perfect_numbers.rb
@@ -11,10 +11,14 @@ class PerfectNumber
 
   def self.calculate_sum
     (1..@number - 1).each do |num|
-      if @number % num == 0
-        @sum += num
-      end
+      @sum += num if @number % num == 0
     end
+
+    #for notes. I like this better using reduce than iterrating with
+    # a sum instance variable
+    # factors = (1...number).select { |num| number % num == 0 }
+    #
+    # result = factors.reduce(:+)
   end
 
   def self.perfect_num
